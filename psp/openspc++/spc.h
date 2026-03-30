e62e8d89ff4536d9f4c9ec3bbaceecc35455afb6
DraxTube01 <draxtube01@gmail.com>
2026-03-30 02:43:10 +0200

    Update spc.h

--- psp/openspc++/spc.h
+++ psp/openspc++/spc.h
@@ -5,11 +5,21 @@
 
 //#include <string.h>
 
+#ifndef uint8
 typedef unsigned char uint8;
+#endif
+#ifndef uint16
 typedef unsigned short uint16;
+#endif
+#ifndef uint32
 typedef unsigned int uint32;
+#endif
+#ifndef int8
 typedef signed char int8;
+#endif
+#ifndef int16
 typedef signed short int16;
+#endif
 //typedef signed int int32;
 
 struct SIAPU
